b47e99f8bde2e9cb991e6fcac9b3dd0817b71c2d
Nika Drozdova <cherepawka13@mail.ru>
2022-11-13 00:15:02 +0300

    add MyActivity2 with implicit intent

--- app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.drozdova.tms.tmsandroidkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_activity_2)
+        btn.setOnClickListener {
+            startActivity(Intent("com.drozdova.tms.tmsandroidkotlin.SECOND"))
+        }
     }
 }
---
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MyActivity2.kt
@@ -0,0 +1,11 @@
+package com.drozdova.tms.tmsandroidkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my2)
+    }
+}
