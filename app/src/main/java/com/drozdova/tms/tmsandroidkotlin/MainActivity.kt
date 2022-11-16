4fd5a146c41f033a8aa7595df1099314e30f4506
Nika Drozdova <cherepawka13@mail.ru>
2022-11-16 18:47:10 +0300

    add homework 17

--- app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.drozdova.tms.tmsandroidkotlin
 
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.EditText
+import com.drozdova.tms.tmsandroidkotlin.MainActivity2.Companion.goToActivity2
 
 class MainActivity : AppCompatActivity() {
+    lateinit var et : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.title = getString(R.string.activity_title_1)
+
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.green)))
+
+        et = findViewById(R.id.edit_text)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        menu?.setGroupVisible(R.id.menu_a2, false)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menu_open_a2 -> goToActivity2(this)
+            R.id.menu_set_in_act_bar -> {
+                val text: String = et.text.toString()
+                goToActivity2(this, text)
+            }
+            R.id.menu_unblock -> {
+                if (et.isEnabled) {
+                    item.title = getString(R.string.menu_unblock)
+                    et.isEnabled = false
+                } else {
+                    item.title = getString(R.string.menu_block)
+                    et.isEnabled = true
+                }
+            }
+
+            R.id.menu_close -> finishAffinity()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MainActivity2.kt
@@ -0,0 +1,61 @@
+package com.drozdova.tms.tmsandroidkotlin
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_2)
+
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(resources.getColor(R.color.orange)))
+
+        val tv1 = findViewById<TextView>(R.id.tv1_a2)
+        val tv2 = findViewById<TextView>(R.id.tv2_a2)
+        tv1.text = intent.getStringExtra(TV_A2_BOLD_TEXT_KEY) ?: "no text"
+        tv2.text = intent.getStringExtra(TV_A2_ITALIC_TEXT_KEY) ?: "no text"
+
+        if (intent.hasExtra(ACTIVITY_TITLE_KEY)) {
+            supportActionBar?.title = intent.getStringExtra(ACTIVITY_TITLE_KEY)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        menu?.setGroupVisible(R.id.menu_a1, false)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.go_back_a1 -> onBackPressed()
+            R.id.menu_close -> finishAffinity()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        private const val ACTIVITY_TITLE_KEY = "A2_TITLE"
+        private const val TV_A2_BOLD_TEXT_KEY = "TV_A_BOLD_2"
+        private const val TV_A2_ITALIC_TEXT_KEY = "TV_A_ITALIC_2"
+
+        fun goToActivity2(context: Context) {
+            val intent = Intent(context, MainActivity2::class.java)
+            intent.putExtra(TV_A2_BOLD_TEXT_KEY, context.getString(R.string.bold_text))
+            intent.putExtra(TV_A2_ITALIC_TEXT_KEY, context.getString(R.string.italic_text))
+            context.startActivity(intent)
+        }
+
+        fun goToActivity2(context: Context, activityTitle: String) {
+            val intent = Intent(context, MainActivity2::class.java)
+            intent.putExtra(ACTIVITY_TITLE_KEY,activityTitle)
+            context.startActivity(intent)
+        }
+    }
+}
