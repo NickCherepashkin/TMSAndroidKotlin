43747a49ce588d59a7e8bf13094d3922cbbcbc4c
Nika Drozdova <cherepawka13@mail.ru>
2022-11-28 19:01:24 +0300

    add classwork

--- app/src/main/java/com/drozdova/tms/tmsandroidkotlin/ItemsFragment.kt
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/ItemsFragment.kt
@@ -6,20 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ItemsFragment : Fragment() {
+    private lateinit var rvFruitAdapter : RecyclerViewAdapter
+//    private lateinit var binding:
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_items, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val listFruits = listOf<String>("Orange", "Banana", "Kiwi", "Apple", "Watermelon")
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.rv_fruits)
+        recyclerView.apply {
+            recyclerView.layoutManager = LinearLayoutManager(activity)
+            rvFruitAdapter = RecyclerViewAdapter()
+            recyclerView.adapter = rvFruitAdapter
+            rvFruitAdapter.setListData(listFruits)
+        }
 
 
     }
