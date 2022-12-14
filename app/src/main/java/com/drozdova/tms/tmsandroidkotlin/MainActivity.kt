5a281ccca9989ca2dc333d5a05efe65263cef61b
Nika Drozdova <cherepawka13@mail.ru>
2022-12-14 11:15:10 +0300

    add viewBinging, add OnBoardingFragment on MainActivity

--- app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MainActivity.kt
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = supportFragmentManager.beginTransaction()
+        fragment.add(R.id.fragments_container, OnBoardingFragment())
+                .commit()
     }
 }
