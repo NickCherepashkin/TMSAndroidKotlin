0dd8ab61170ed01fd0bf1acf538273f7be892e88
Nika Drozdova <cherepawka13@mail.ru>
2023-02-28 21:00:27 +0300

    added location

--- app/src/main/java/com/drozdova/tms/tmsandroidkotlin/presentation/MainActivity.kt
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/presentation/MainActivity.kt
@@ -1,19 +1,28 @@
 package com.drozdova.tms.tmsandroidkotlin.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.NavGraph
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.drozdova.tms.tmsandroidkotlin.R
 import com.drozdova.tms.tmsandroidkotlin.databinding.ActivityMainBinding
 import com.drozdova.tms.tmsandroidkotlin.presentation.viewmodel.MainViewModel
+import com.drozdova.tms.tmsandroidkotlin.utils.permissions.PermissionUtils
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -85,4 +94,79 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         navController.removeOnDestinationChangedListener(this)
         super.onDestroy()
     }
+
+    override fun onStart() {
+        super.onStart()
+        when {
+            PermissionUtils.checkAccessFineLocationGranted(this) -> {
+                when {
+                    PermissionUtils.isLocationEnabled(this) -> {
+                        setLocationListner()
+                    }
+                    else -> {
+                        PermissionUtils.showGPSNotEnabledDialog(this)
+                    }
+                }
+            }
+            else -> {
+                PermissionUtils.askAccessFineLocationPermission(
+                    this,
+                    42
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            42 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    when {
+                        PermissionUtils.isLocationEnabled(this) -> {
+                            setLocationListner()
+                        }
+                        else -> {
+                            PermissionUtils.showGPSNotEnabledDialog(this)
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "not granded permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun setLocationListner() {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult) {
+                    super.onLocationResult(locationResult)
+                    for (location in locationResult.locations) {
+                        Log.w("location", "${location.latitude} ${location.longitude}")
+                    }
+                }
+            },
+            Looper.myLooper()
+        )
+    }
+
 }
--- app/src/main/java/com/drozdova/tms/tmsandroidkotlin/presentation/view/home/items/FavouriteFragment.kt
+++ app/src/main/java/com/drozdova/tms/tmsandroidkotlin/presentation/view/home/items/FavouriteFragment.kt
@@ -1,10 +1,20 @@
 package com.drozdova.tms.tmsandroidkotlin.presentation.view.home.items
 
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
+import android.location.LocationManager.GPS_PROVIDER
+import android.location.LocationManager.NETWORK_PROVIDER
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.getSystemService
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.drozdova.tms.tmsandroidkotlin.R
@@ -14,6 +24,7 @@ import com.drozdova.tms.tmsandroidkotlin.presentation.view.home.items.adapter.Fa
 import com.drozdova.tms.tmsandroidkotlin.presentation.viewmodel.FavouriteItemsViewModel
 import com.drozdova.tms.tmsandroidkotlin.presentation.viewmodel.ItemsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.Exception
 
 @AndroidEntryPoint
 class FavouriteFragment : Fragment() {
@@ -45,5 +56,30 @@ class FavouriteFragment : Fragment() {
         viewModel.fav.observe(viewLifecycleOwner) {favList ->
             favAdapter.submit(favList)
         }
+
+        var locationMmanager: LocationManager? = null
+
+        locationMmanager = requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager?
+
+        try {
+            if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                return
+            }
+            locationMmanager?.requestLocationUpdates(
+                    NETWORK_PROVIDER,
+                    0L,
+                    0f,
+                    locationListener
+            )
+        } catch (e: Exception) {
+
+        }
     }
+
+    private val locationListener = LocationListener{
+
+        Toast.makeText(requireContext(), "Long: ${it.longitude} lat ${it.latitude}", Toast.LENGTH_LONG).show()
+    }
+
 }
